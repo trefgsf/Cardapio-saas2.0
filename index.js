0ea7b3225d0cafbaef2d6189f9342f1d5624a3b0
trefgsf <netotrevor14@gmail.com>
2026-09-24 19:36:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const authRoutes = require('./routes/auth');
 const painelRoutes = require('./routes/painel');
+const publicRoutes = require('./routes/public');
 require('dotenv').config();
 
 const app = express();
